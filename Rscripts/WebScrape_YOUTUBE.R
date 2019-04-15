9424dffe56bf9f799f931dfb1b51b6e2617f4583
Shreeman Arvind <shreemanarvind@Lalitendus-MacBook-Air.local>
2019-04-16 01:38:15 +0530

    pel diya youtube

--- Rscripts/WebScrape_YOUTUBE.R
+++ Rscripts/WebScrape_YOUTUBE.R
@@ -1,20 +1,24 @@
+#install.packages("RSelenium")
 #install.packages("rvest")
 #install.packages("stringr")
 library(rvest)
 library(stringr)
-
+library(RSelenium)
 url <- "https://www.youtube.com/watch?v=kJQP7kiw5Fk"
-
-html <- read_html(url)
-
+driver<- rsDriver(browser=c("chrome"))
+remDr <- driver[["client"]]
+help(read_html)
+page <- "rtestyt.html"
+html <- read_html(page)
+html
 get_comments <- function(html){
   html %>%
-  html_nodes(".yt-formatted-string") %>%   
+  html_nodes("#content-text") %>%   
   html_text() %>%
   str_trim() %>%
   unlist()
 }
 
 comments <- get_comments(html)
-comments
+
 
