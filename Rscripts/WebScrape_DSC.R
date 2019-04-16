7217a04a03a62662577ca49f96adf0a80d26e4b2
kunal15abc <kunal15abc@gmail.com>
2019-04-17 02:30:11 +0530

    Code to exract table data

--- Rscripts/WebScrape_DSC.R
+++ Rscripts/WebScrape_DSC.R
@@ -1,41 +1,52 @@
-#install.packages("rvest")
-#install.packages("stringr")
+# install.packages("rvest")
+# install.packages("stringr")
 library(rvest)
 library(stringr)
 
+#SCRIPT VARIABLES
+
+# Provide URL -
 url <- "https://www.drugshortagescanada.ca/?short=50"
 
-html <- read_html(url)
+# Provide Table ID -
+tableID <- "drug-shortage-reports"
 
-get_comments <- function(html){
+# Provide output Filename -
+outputFileName<- "drug-shortage.csv"
+
+
+html <- read_html(url)
+cssSelectoHead <- paste("#",tableID," thead tr th", sep = "")
+cssSelectoBody <- paste("#",tableID," tbody tr td", sep = "")
+
+getTableHeader <- function(html){
   html %>%
-  html_nodes("#drug-shortage-reports tbody tr td") %>%   
+    html_nodes(cssSelectoHead) %>%   
+    html_text() %>%
+    str_trim() %>%
+    unlist()
+}
+
+getTableData <- function(html){
+  html %>%
+  html_nodes(cssSelectoBody) %>%   
   html_text() %>%
   str_trim() %>%
   unlist()
 }
 
-comments <- get_comments(html)
-comments
+tableHeader <- getTableHeader(html)
+colCount <- length(tableHeader)
 
+tableData <- getTableData(html)
+rowCount <- length(tableData)/colCount
+rowCount
 
+dataTable <- data.frame(matrix(ncol = colCount, nrow= rowCount))
+colnames(dataTable) <- tableHeader
 
+for(n in 1:7) {
+  dataTable[,n] <- tableData[seq(n,length(tableData),colCount)]
+}
 
-
-
-count = 1
-col1 <- sapply(comments, function(i) {
-  if (mod(count,7) == 1){
-    print(i)
-  }
-  count = count + 1
-})
-
-
-length(new_vector)
-head(new_vector)
-class(new_vector)
-
-
-table <- breakArray(comment)
-print(tab)
+write.csv(dataTable,file=outputFileName, row.names = FALSE)
